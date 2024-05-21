e1d590709e6528c39f60fd4c78931a189048b815
hwshawn <hwshawn@disclassics.net>
2024-05-21 08:40:54 +0800

    051224, morning(1)

---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjDEMO.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Homework1()
+        {
+            return View();
+        }
+    }
+}
